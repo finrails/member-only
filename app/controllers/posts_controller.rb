cfd4e6d4f473dce7a7dc8f7a0e7d8ce7e31af9f1
finrails <macacofinn@yahoo.com>
2022-05-10 17:23:07 -0400

    upt: post controller and success flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:success] = 'Your post have been submited!'
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
