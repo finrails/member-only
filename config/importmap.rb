20f21f58ca87f3a096b56587b99808058af40a25
finrails <macacofinn@yahoo.com>
2022-05-12 09:58:05 -0400

    add: javascript cash lib

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "cash", to: "https://cdn.jsdelivr.net/npm/cash-dom@8.1.1/dist/cash.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
