a5228970c60371683fa7e1e581c8f3228d2d633d
finrails <macacofinn@yahoo.com>
2022-05-12 15:36:13 -0400

    add: close alert function

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "cash", to: "https://cdn.jsdelivr.net/npm/cash-dom@8.1.1/dist/cash.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/utils", under: "utils"
+pin_all_from "app/javascript/utils", under: "utils", preload: true
 
